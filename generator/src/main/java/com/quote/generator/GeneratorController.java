9c8ad10de50172a1f37c146aca0fee9b701ef57d
Kashish <kashishverma8382@gmail.com>
2025-03-29 15:07:42 +0530

    add data

--- generator/src/main/java/com/quote/generator/GeneratorController.java
+++ generator/src/main/java/com/quote/generator/GeneratorController.java
@@ -1,2 +1,33 @@
-package com.quote.generator;public class GeneratorController {
+package com.quote.generator;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/quote")
+public class GeneratorController {
+    @GetMapping("/test")
+    public String fun(){
+    ArrayList<String> test=new ArrayList<String>();
+    test.add("Hello");
+    test.add("Quote");
+    Random random=new Random();
+    int k=random.nextInt(test.size());
+
+    StringBuilder s=new StringBuilder();
+
+    for(int i=0;i<5;i++){
+        s.append("s");
+        test.add(s.toString());
+
+    }
+
+
+        return test.get(k);
+    }
+
 }
+
