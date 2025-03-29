82adbeafd44ff56824bda2fc741d38f2176d1663
Abhimanyu <abhimanyusinghanand02@gmail.com>
2025-03-29 15:37:59 +0530

    Added a post request

--- generator/src/main/java/com/quote/generator/GeneratorController.java
+++ generator/src/main/java/com/quote/generator/GeneratorController.java
@@ -1,21 +1,23 @@
 package com.quote.generator;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.*;
 import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/quote")
 public class GeneratorController {
+    ArrayList<Quote> quotes = new ArrayList<Quote>();
     @GetMapping("/test")
     public String fun(){
     ArrayList<String> test=new ArrayList<String>();
     test.add("Hello");
     test.add("Quote");
     Random random=new Random();
-    int k=random.nextInt(test.size());
+
 
     StringBuilder s=new StringBuilder();
 
@@ -25,8 +27,17 @@ public class GeneratorController {
 
     }
 
-
-        return test.get(k);
+        int k=random.nextInt(quotes.size());
+        return quotes.get(k).getName();
+    }
+    @PostMapping("/add")
+    public ResponseEntity<String> addItem(@RequestBody Quote item) {
+        // Simulate saving the item (e.g., to a database)
+        System.out.println("Received item: " + item);
+        quotes.add(item);
+        // Return a success response
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Item added successfully!");
     }
 
 }
